693a017ea515cb08fca0aac902c740aa1bcd8086
Chrystine Martins <chrystine.martins@outlook.com>
2023-11-14 20:27:06 -0300

    adicionando css

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ let nome = prompt("Digite o nome do seu personagem.")
 let nivel = prompt("Digite o level do seu personagem.")
 
 if(nivel < 1000){
-  console.log("O (A) Herói (Heroína) de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Ferro")
+  alert("O (A) Herói (Heroína) de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Ferro")
 }
 
 else if(nivel <= 2000){
-  console.log("O (A) Herói (Heroína) de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Bronze")
+  alert("O (A) Herói (Heroína) de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Bronze")
 }
 
 else if(nivel <= 5000){
@@ -14,19 +14,19 @@ else if(nivel <= 5000){
 }
 
 else if(nivel <= 7000){
-  console.log("O (A) Herói (Heroína) de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Ouro")
+  alert("O (A) Herói (Heroína) de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Ouro")
 }
 
 else if(nivel <= 8000){
-  console.log("O (A) Herói (Heroína) de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Platina")
+  alert("O (A) Herói (Heroína) de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Platina")
 }
 
 else if(nivel <= 9000){
-  console.log("O (A) Herói (Heroína) de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Ascendente")
+  alert("O (A) Herói (Heroína) de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Ascendente")
 }
 
 else if (nivel <= 10000) {
-  console.log("O (A) Herói (Heroína) de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Imortal")
+  alert("O (A) Herói (Heroína) de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Imortal")
 }
 
 else {
