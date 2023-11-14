f4b855b5b4dfdc77b7b00da2f9a9cd6fcacc2367
Chrystine Martins <chrystine.martins@outlook.com>
2023-11-14 19:13:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let nome = "Chrystine"
+let nivel = 10001
+
+if(nivel < 1000){
+  console.log("A heroína de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Ferro")
+}
+
+else if(nivel <= 2000){
+  console.log("A heroína de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Bronze")
+}
+
+else if(nivel <= 5000){
+  console.log("A heroína de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Prata")
+}
+
+else if(nivel <= 7000){
+  console.log("A heroína de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Ouro")
+}
+
+else if(nivel <= 8000){
+  console.log("A heroína de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Platina")
+}
+
+else if(nivel <= 9000){
+  console.log("A heroína de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Ascendente")
+}
+
+else if (nivel <= 10000) {
+  console.log("A heroína de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Imortal")
+}
+
+else {
+  console.log("A heroína de nome " + nome + " está no Nível " + nivel + ", na hierarquia: Radiante")
+}
