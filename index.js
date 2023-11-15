7d6ef5f8ea605d9c2c17f7e7af3a4b777d5eca5e
Chrystine Martins <chrystine.martins@outlook.com>
2023-11-14 22:03:46 -0300

    update js

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 let hero = prompt("Enter your character's name.")
 let level = prompt("Enter your character's level.")
 
-if(nivel < 1000){
+if(level < 1000){
   alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Iron.")
 }
 
-else if(nivel <= 2000){
+else if(level <= 2000){
   alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Bronze.")
 }
 
-else if(nivel <= 5000){
+else if(level <= 5000){
   alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Silver.")
 }
 
-else if(nivel <= 7000){
+else if(level <= 7000){
   alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Gold.")
 }
 
-else if(nivel <= 8000){
+else if(level <= 8000){
   alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Platinum.")
 }
 
-else if(nivel <= 9000){
+else if(level <= 9000){
   alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Ascending.")
 }
 
-else if (nivel <= 10000) {
+else if (level <= 10000) {
   alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Bright.")
 }
 
