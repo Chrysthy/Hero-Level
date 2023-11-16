df4e96d64832752cb569455160c7ba5ce2cdc3db
Chrystine Martins <chrystine.martins@outlook.com>
2023-11-16 20:48:38 -0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Hero Level
+
+### This project was created during the Potência Tech iFood Bootcamp - Programação do Zero.
+
+![Captura de tela 2023-11-14 205212](https://github.com/Chrysthy/Hero-Level/assets/126017173/8e395cf8-6cb8-4212-b897-34d9cfb300ca)
--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-let hero = prompt("Enter your character's name.")
-let level = prompt("Enter your character's level.")
+setTimeout(function () {
 
-if(level < 1000){
-  alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Iron.")
-}
 
-else if(level <= 2000){
-  alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Bronze.")
-}
+  let hero = prompt("Enter your character's name.")
+  let level = prompt("Enter your character's level.")
 
-else if(level <= 5000){
-  alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Silver.")
-}
+  if (level < 1000) {
+    alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Iron.")
+  }
 
-else if(level <= 7000){
-  alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Gold.")
-}
+  else if (level <= 2000) {
+    alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Bronze.")
+  }
 
-else if(level <= 8000){
-  alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Platinum.")
-}
+  else if (level <= 5000) {
+    alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Silver.")
+  }
 
-else if(level <= 9000){
-  alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Ascending.")
-}
+  else if (level <= 7000) {
+    alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Gold.")
+  }
 
-else if (level <= 10000) {
-  alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Bright.")
-}
+  else if (level <= 8000) {
+    alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Platinum.")
+  }
 
-else {
-  alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Immortal.")
-}
+  else if (level <= 9000) {
+    alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Ascending.")
+  }
+
+  else if (level <= 10000) {
+    alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Bright.")
+  }
+
+  else {
+    alert("The hero, named " + hero + ", is at Level " + level + ", within the hierarchy: Immortal.")
+  }
+}, 300);
